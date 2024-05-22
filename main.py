eccd704196207fe8f1967a7bf5dd600de7a54446
Mauricio Perez <mauricio.perez24@itca.edu.sv>
2024-05-22 09:22:49 -0600

    agragado elementos

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from utilidades import *
 
 # Función principal
 def main():
+  """
+  Esta función es el punto de entrada del programa.
+  Solicita al usuario que inicie sesión y redirige a la sesión correspondiente según las credenciales proporcionadas.
+  """
   print("\t\tBienvenido a la tienda\n")
   print("\t\tPor favor inicie sesión\n")
   
   usuario = input("Usuario: ")
   contrasena = input("\nContraseña: ")
+
   if usuario == "admin" and contrasena == "1234":
     print("\nSesión iniciada correctamente\n")
     sesionAdmin()
@@ -16,6 +21,6 @@ def main():
     sesionVendedor()
   else:
     print('Nombre de usuario o contraseña incorrectos')
-    main()  
+    main()
 if __name__ == "__main__":
   main()
--- utilidades.py
+++ utilidades.py
@@ -2,8 +2,11 @@ import os
 import pandas as pd
 import datetime
 
-# Mantener la sesion activa
 def sesionActivaAdmin():
+  """
+  Función que mantiene la sesión activa para el administrador.
+  Permite al administrador salir del programa o volver al menú principal.
+  """
   sesion = input('\n¿Quieres salir? \n (1) Si \n (2) No \n')
   if sesion == '1':
     print('Gracias por usar el programa')
@@ -13,6 +16,10 @@ def sesionActivaAdmin():
     sesionAdmin()
 
 def sesionActivaVendedor():
+  """
+  Función que mantiene la sesión activa para el vendedor.
+  Permite al vendedor salir del programa o volver al menú principal.
+  """
   sesion = input('\n¿Quieres salir? \n (1) Si \n (2) No \n')
   if sesion == '1':
     print('Gracias por usar el programa')
@@ -20,17 +27,21 @@ def sesionActivaVendedor():
   else:
     os.system('cls')
     sesionVendedor()
-# <------------------------------------Sesion admin---------------------------------->
+
 def sesionAdmin():
+  """
+  Función que muestra el menú principal y las opciones disponibles para el administrador.
+  Permite al administrador seleccionar una opción y realizar las acciones correspondientes.
+  """
   print("\t\tBienvenido a la tienda\n")
   print("\t\tSeleccione una opción:")
   print("\n(1) Registrar producto"
-        "\n(2) Listar productos"
-        "\n(3) Buscar producto"
-        "\n(4) Eliminar producto"
-        "\n(5) Modificar producto"
-        "\n(6) Generar reporte"
-        "\n(7) Salir\n"
+      "\n(2) Listar productos"
+      "\n(3) Buscar producto"
+      "\n(4) Eliminar producto"
+      "\n(5) Modificar producto"
+      "\n(6) Generar reporte"
+      "\n(7) Salir\n"
   )
   try:
     opcion = int(input('Elige una opción: '))
@@ -68,15 +79,18 @@ def sesionAdmin():
     os.system('cls')
     sesionAdmin()
 
-# <------------------------------------Sesion vendedor------------------------------->
 def sesionVendedor():
+  """
+  Función que muestra el menú principal y las opciones disponibles para el vendedor.
+  Permite al vendedor seleccionar una opción y realizar las acciones correspondientes.
+  """
   print("\t\tBienvenido a la tienda\n")
   print("\t\tSeleccione una opción:")
   print("\n(1) Listar producto producto"
-        "\n(2) Buscar productos"
-        "\n(3) Registrar una venta"
-        "\n(4) Salir\n"
-      )
+      "\n(2) Buscar productos"
+      "\n(3) Registrar una venta"
+      "\n(4) Salir\n"
+  )
   try:
     opcion = int(input('Elige una opción: '))
     os.system('cls')
@@ -104,21 +118,21 @@ def sesionVendedor():
     os.system('cls')
     sesionVendedor()
 
-# <-----------------------------Registrar un producto-------------------------------->
 def registrarProducto():
-  # Abrir el archivo en modo escritura
+  """
+  Función que registra un nuevo producto.
+  Solicita al usuario ingresar los datos del producto y los guarda en un archivo CSV.
+  """
   archivo = open("productos.csv", "a")
+  
   try:
-    # Pedir los datos del producto
     nombreProducto = str(input("Ingrese el nombre del producto: "))
     precioProducto = float(input("Ingrese el precio unitario producto: "))
     cantidadProducto = int(input("Ingrese la cantidad del producto: "))
 
-    # Realizar la operación
     precioTotal = precioProducto * cantidadProducto
     fechaActual = datetime.date.today().strftime( "%d/%m/%Y")
 
-    # Escribir los datos en el archivo
     archivo.write(nombreProducto + "," + str(fechaActual) + "," + str(precioProducto) + "," + str(precioTotal) + "," + str(cantidadProducto) + "," + ","+ str(cantidadProducto) + "\n")
     archivo.close()
   except:
@@ -127,42 +141,40 @@ def registrarProducto():
     os.system('cls')
     registrarProducto()
 
-# <-------------------------Listar los productos registrados------------------------>
 def listarProductos():
+  """
+  Función que lista todos los productos registrados.
+  Lee los datos del archivo CSV y muestra la información en forma de tabla.
+  """
   df = pd.read_csv('productos.csv')
   print(df.to_string(index=False))
 
-
-# <-----------------------------Buscar un producto---------------------------------->
 def buscarProducto():
+  """
+  Función que busca un producto por su nombre.
+  Lee los datos del archivo CSV y muestra la información del producto si es encontrado.
+  """
   try:
-    # Abrir el archivo en modo lectura
-    archivo = open("productos.csv", "r")
     df = pd.read_csv("productos.csv")
     
-    # Pedir el nombre del producto a buscar
     nombreProducto = str(input("Ingrese el nombre del producto a buscar: "))
     
-    # Buscar el producto en el DataFrame
     producto = df.iloc[df.index[df['NOMBRE'] == nombreProducto].tolist()[0], :]
     
     if not producto.empty:
-        # Imprimir los encabezados con más espacios
-        print(f"{'NOMBRE':<20}{'FECHA':<20}{'PRECIO UNITARIO':<20}{'PRECIO TOTAL':<20}{'ENTRADAS':<20}{'SALIDAS':<20}{'STOCK':<20}")
-        
-        # Imprimir la información del producto con más espacios
-        print(f"{producto['NOMBRE']:<20}{producto['FECHA']:<20}{producto['PRECIO UNITARIO']:<20}{producto['PRECIO TOTAL']:<20}{producto['ENTRADAS']:<20}{producto['SALIDAS']:<20}{producto['STOCK']:<20}")
+      print(f"{'NOMBRE':<20}{'FECHA':<20}{'PRECIO UNITARIO':<20}{'PRECIO TOTAL':<20}{'ENTRADAS':<20}{'SALIDAS':<20}{'STOCK':<20}")
+      print(f"{producto['NOMBRE']:<20}{producto['FECHA']:<20}{producto['PRECIO UNITARIO']:<20}{producto['PRECIO TOTAL']:<20}{producto['ENTRADAS']:<20}{producto['SALIDAS']:<20}{producto['STOCK']:<20}")
     else:
-        print("El Producto no fue encontrado")
+      print("El Producto no fue encontrado")
   except Exception as e:
     print("El Producto no fue encontrado")
-    print(f"Error: {e}")
-  finally:
-    archivo.close()
+    print(f"Error: {e}") 
 
-
-# <-----------------------------Eliminar un producto-------------------------------->
 def eliminarProducto():
+  """
+  Función que elimina un producto por su nombre.
+  Lee los datos del archivo CSV, busca el producto y lo elimina del DataFrame.
+  """
   df = pd.read_csv('productos.csv')
 
   nombreProducto = str(input('Nombre del producto: '))
@@ -174,17 +186,19 @@ def eliminarProducto():
   except Exception as e:
     print('Producto no encontrado')
     print(f'Error: {e}')
-# <-----------------------------Registrar salida-------------------------------------->
+
 def registrarSalida():
+  """
+  Función que registra una salida de productos.
+  Lee los datos del archivo CSV, actualiza la cantidad de salidas y el stock del producto.
+  """
   df = pd.read_csv('productos.csv')
 
   nombreProducto = str(input('Nombre del prodcuto: '))
   cantidadProducto = str(input(f'Numero de {nombreProducto}s a vender: '))
 
   indice = df.index[df['NOMBRE'] == nombreProducto].tolist()[0]
-  #df.loc[indice,'SALIDAS'] = cantidadProducto
 
-  #Capturar y almacenar en variables los datos del archivo productos.csv
   entradaProducto = df.iloc[indice,4]
   salidaProducto = df.iloc[indice,5]
   stockActual = df.iloc[indice,6]
@@ -192,11 +206,9 @@ def registrarSalida():
   if int(stockActual) < int(cantidadProducto):
     print(f'La cantidad de {nombreProducto}s no es suficente para consilidar la venta\n')
   else:
-    #actualizando datos del archivo productos
     stock = int(stockActual) - int(cantidadProducto)
     actualizacionSalida = int(salidaProducto) + int(cantidadProducto)
 
-    #guardar los cambios
     df.loc[indice,'SALIDAS'] = actualizacionSalida
     df.loc[indice,'STOCK'] = stock
     df.to_csv('productos.csv',index=False)
